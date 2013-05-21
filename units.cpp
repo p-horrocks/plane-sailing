c582f63882776a171c1df028ea49cb93137bd070
Peter Horrocks <p.horrocks@marathon-targets.com>
2013-05-21 17:50:02 +1000

    Not using strptime because it doesn't exist on windows

--- units.cpp
+++ units.cpp
@@ -2,13 +2,6 @@
 
 #include <cstring>
 
-namespace
-{
-
-const char* TIME_FORMAT = "%H:%M:%S";
-
-} // namespace
-
 double FeetToMetres(double Feet)
 {
     return Feet * 0.3048;
@@ -34,15 +27,15 @@ double MPSToKnots(double MPS)
     return MPS * 3600.0 / 1852.0;
 }
 
-time_t stringToTime(const QString& timeStr)
+double stringToTime(const QString& timeStr)
 {
     return stringToTime(timeStr.toAscii().data());
 }
 
-time_t stringToTime(const char* time)
+double stringToTime(const char* time)
 {
-    struct tm t;
-    memset(&t, 0, sizeof(t));
-    strptime(time, TIME_FORMAT, &t);
-    return mktime(&t);
+    // strptime doesn't exist on Windows
+    int hrs, min, sec;
+    sscanf(time, "%d:%d:%d", &hrs, &min, &sec);
+    return (hrs * 3600) + (min * 60) + sec;
 }
